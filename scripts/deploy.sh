4ecd3f1fc4796ec6be4ecaed8af04d9b9ba7b879
jayh36 <jhong0306@naver.com>
2021-02-09 18:32:20 +0900

    Modify

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl demo | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -f demo.*.jar)
 
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
